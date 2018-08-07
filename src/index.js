13537d125dbc6484f954c3cb1c9b2b5b1c97f0ec
roman <r.levyst@gmail.com>
2018-08-07 10:48:08 -0400

    use script loader to expose jquery globally, add verisoning

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-console.log(`I've been required by Webpack`);
-
 import style from "./_sass/main.scss";
 
-// import jQuery from './js/lib/jquery-2.2.2.min.js';
-// import anchorJump from './js/lib/jquery/anchorjump-1.0.min.js';
-// import waypoints from './js/lib/jquery.waypoints.min.js';
+import 'script-loader!./js/lib/jquery-2.2.2.min.js';
+import './js/lib/jquery.anchorjump-1.0.min.js';
+import './js/lib/jquery.waypoints.min.js';
 
-// import app from './js/app.js';
+import app from './js/app.js';
