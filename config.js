17a1a7f089b11404857c7532c52d27b1b77be769
KISS <me@luisam.com>
2018-08-08 12:55:59 -0400

    fixed lint issue in config

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
 const config = {
-    site: {
-      title: "insert_site_name",
-      description: "insert_site_description",
-      baseurl: "/",
-      url: "http://example.mlh.io",
-      custom_class: "",
-      favicon_url: "img/favicon.ico"
-    },
-    social: {
-      facebook_image_url: "",
-      twitter_image_url: ""
-    },
-    tracking: {
-      google_analytics_id: "UA-XXXXXXXX-XX",
-      twitter_id: "XXXXX",
-      facebook_id: "XXXXXXXXXXXXXXXX",
-    }
+  site: {
+    title: 'insert_site_name',
+    description: 'insert_site_description',
+    baseurl: '/',
+    url: 'http://example.mlh.io',
+    custom_class: '',
+    favicon_url: '/src/img/favicon.ico'
+  },
+  social: {
+    facebook_image_url: '',
+    twitter_image_url: ''
+  },
+  tracking: {
+    google_analytics_id: 'UA-XXXXXXXX-XX',
+    twitter_id: 'XXXXX',
+    facebook_id: 'XXXXXXXXXXXXXXXX'
+  }
 }
 
-module.exports = config;
+module.exports = config
